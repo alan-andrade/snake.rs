0d7845a047a8ac916d9ed2f5e3eb3ee937b44003
Alan Andrade <alan.andradec@gmail.com>
2014-05-30 19:17:53 -0700

    stage setup and basic movement

--- snake.rs
+++ snake.rs
@@ -5,10 +5,6 @@ use ncurses::*;
 
 static KEY_Q: i32 = 'q' as i32;
 
-enum Direction {
-    Up, Down, Left, Right
-}
-
 struct Position {
     x: i32,
     y: i32
@@ -21,26 +17,80 @@ impl Position {
     fn right(&mut self) { self.x = self.x+1; }
 }
 
+static WALL: &'static str = "#";
+
+struct Stage {
+    cols: i32,
+    rows: i32
+}
+
+impl Stage {
+    fn new(cols: i32, rows: i32) -> Stage {
+        Stage { cols: cols, rows: rows }
+    }
+
+    fn draw_walls(&self) {
+        for x in range(0, self.cols) {
+            move(0, x);
+            printw(WALL);
+            move(self.rows, x);
+            printw(WALL);
+        }
+
+        for y in range(0, self.rows) {
+            move(y, 0);
+            printw(WALL);
+            move(y, self.cols);
+            printw(WALL);
+        }
+    }
+
+    fn center(&self) -> (i32,i32) {
+        (self.cols / 2, self.rows / 2)
+    }
+}
+
+struct Snake {
+    pos: Position,
+    symbol: &'static str
+}
+
+impl Snake {
+    fn new (stage: &Stage) -> Snake {
+        let (x,y) = stage.center();
+        Snake {
+            pos: Position { x: x, y: y },
+            symbol: "@"
+        }
+    }
+
+    fn draw (&mut self) {
+        move(self.pos.y, self.pos.x);
+        printw(self.symbol);
+    }
+}
+
 fn main () {
     initscr();
     noecho();
     curs_set(CURSOR_INVISIBLE);
     keypad(stdscr, true);
 
-    let mut pos = Position{ x:0, y:0 };
+    let stage = Stage::new(43, 31);
+    let mut snake = Snake::new(&stage);
 
     loop {
+        stage.draw_walls();
+        snake.draw();
         match getch() {
-            KEY_UP      => { pos.up();      },
-            KEY_DOWN    => { pos.down();    },
-            KEY_LEFT    => { pos.left();    },
-            KEY_RIGHT   => { pos.right();   },
+            KEY_UP      => { snake.pos.up();      },
+            KEY_DOWN    => { snake.pos.down();    },
+            KEY_LEFT    => { snake.pos.left();    },
+            KEY_RIGHT   => { snake.pos.right();   },
             KEY_Q       => { break; }
             _ => {}
         }
         clear();
-        move(pos.y, pos.x);
-        printw("#");
         refresh();
     }
 
